7bad15506e74d12abae1cd4ac9d7c83cd92607aa
Marat <mb@bestlife.ag>
2015-06-08 19:48:22 +0200

    init

---
+++ application/translations/de/vskfrei_lang.php
@@ -0,0 +1,10 @@
+<?php
+
+// -------------------------------
+// RESOURCE IDENTITFIER = STRING
+// -------------------------------
+$aLang = array(
+    'charset'                      => 'UTF-8',
+    'FREE_SHIPPING_OVER'           => 'Ab %s EUR Warenwert übernehmen wir die Versandkosten!',
+    'ORDER_MORE_FOR_FREE_SHIPPING' => 'Bestellen Sie für nur <b>%s EUR</b> mehr für den kostenlosen Versand.'
+);
---
+++ application/views/admin/de/module_options.php
@@ -0,0 +1,8 @@
+<?php
+
+$aLang = array(
+    'charset'                        => 'UTF-8',
+    'SHOP_MODULE_GROUP_vskfreiMain'  => '<style type="text/css">.groupExp a.rc b {font-size: medium; color: #ff3600; }.groupExp dt input.txt { width: 430px !important} .groupExp dl { display: block !important; } input.confinput {position: fixed; top: 20px; right: 70px; background: #008B2D; padding: 10px 25px; color: white; border: 1px solid black; cursor:pointer; font-size: 125%; } input.confinput:hover {outline: 3px solid #ff3600;} .groupExp dt textarea.txtfield {width: 430px; height: 150px;}</style> Einstellungen',
+    'SHOP_MODULE_iBlaVSKfreiAb'      => 'VSK frei ab ...EUR',
+    'SHOP_MODULE_iBlaVSKfreiAbStart' => 'Der Hinweis wird erst ab diesem Warenkorb-Wert angezeigt'
+);
