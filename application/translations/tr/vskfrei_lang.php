218454a7d218f108e205911ab3adce8c87ca7140
Marat <vanilla-thunder@users.noreply.github.com>
2018-03-02 11:11:59 +0100

    Add files via upload

---
+++ application/translations/tr/vskfrei_lang.php
@@ -0,0 +1,12 @@
+<?php
+
+// -------------------------------
+// RESOURCE IDENTITFIER = STRING
+// -------------------------------
+$sLangName  = 'Türkçe';
+$aLang = array(
+    'charset'                      => 'UTF-8',
+    'FREE_SHIPPING_OVER'           => '%s EURO Tutarı Üzerinde Ücretsiz Kargo!',
+    'ORDER_MORE_FOR_FREE_SHIPPING' => 'Sadece <b> %s EURO</b> Tutarındaki Siparişinizde Ücretsiz Kargo',
+	'YOU_SAVE_SHIPPING_COSTS'      => '<b>%s EURO</b> Kargo Bedelinden Tasarruf Edersiniz'
+);
